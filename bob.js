3f43026019492135eacf711364d3d7d855d35bc7
Aditi13-maker <72981055+Aditi13-maker@users.noreply.github.com>
2020-12-02 19:40:42 +0530

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,32 @@
+class Bob{
+    constructor(x,y,diameter){
+        var options = {
+            isStatic:false,
+            restitution:1,
+            friction:0,
+            density:1.2
+        }
+        this.x=x
+        this.y=y;
+        this.diameter=diameter
+        this.body=Bodies.circle(this.x,this.y,this.diameter/2,options)
+        World.add(world, this.body);
+    }
+    display(){
+        var paperpos =this.body.position;
+        
+        push();
+        translate(paperpos.x, paperpos.y);
+        rectMode(CENTER);
+        strokeWeight(1);
+        stroke("white")
+        fill("black");
+        ellipse(0, 0, this.r, this.r);
+        pop();
+      }
+    
+
+
+
+
+}
